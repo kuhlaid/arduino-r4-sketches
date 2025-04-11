23bc2d9c7ce80895c1f726826d42281434da53f6
w. Patrick Gale <41551429+kuhlaid@users.noreply.github.com>
2025-04-11 10:07:42 -0400

    Create WiFi-MacAddress.ino

---
+++ sketch/WiFi-MacAddress.ino
@@ -0,0 +1,34 @@
+/*
+This sketch simply outputs the MAC (hardware) address of the device to the serial monitor. Useful if you need the address to register an R4 with DHCP.
+*/
+#include <WiFiS3.h>
+
+void setup() {
+
+  Serial.begin(115200);
+  while (!Serial) ;
+  Serial.println("START");
+
+  WiFi.begin("dummy");
+
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC Address: ");
+  printMacAddress(mac);
+}
+
+void loop() {
+}
+
+void printMacAddress(byte mac[]) {
+  for (int i = 0; i < 6; i++) {
+    if (i > 0) {
+      Serial.print(":");
+    }
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+  }
+  Serial.println();
+}
